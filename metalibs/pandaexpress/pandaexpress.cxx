54ee640d7024b97e60d527444ac8e5bcb896174a
Brian Lach <brianlach72@gmail.com>
2021-10-14 21:25:48 -0400

    export a symbol for stub libpandaexpress.dll, initialize components

--- metalibs/pandaexpress/pandaexpress.cxx
+++ metalibs/pandaexpress/pandaexpress.cxx
@@ -3,3 +3,27 @@
  * @author drose
  * @date 2000-05-15
  */
+
+#include "dconfig.h"
+#include "config_express.h"
+#include "config_downloader.h"
+
+#if defined(_WIN32) && !defined(CPPPARSER) && !defined(LINK_ALL_STATIC)
+__declspec(dllexport)
+#endif
+void
+init_libpandaexpress() {
+  static bool initialized = false;
+  if (initialized) {
+    return;
+  }
+  initialized = true;
+
+  init_libexpress();
+  init_libdownloader();
+}
+
+Configure(config_pandaexpress);
+ConfigureFn(config_pandaexpress) {
+  init_libpandaexpress();
+}
