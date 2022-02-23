c9a7c09f3dadb40580fc16a8f16a575b7bb12ecf
Brian Lach <brianlach72@gmail.com>
2022-02-22 19:49:00 -0500

    stub triangulatorDelaunay3

---
+++ src/mathutil/triangulatorDelaunay3.h
@@ -0,0 +1,28 @@
+/**
+ * PANDA 3D SOFTWARE
+ * Copyright (c) Carnegie Mellon University.  All rights reserved.
+ *
+ * All use of this software is subject to the terms of the revised BSD
+ * license.  You should have received a copy of this license along
+ * with this source code in a file named "LICENSE."
+ *
+ * @file triangulatorDelaunay3.h
+ * @author brian
+ * @date 2021-12-09
+ */
+
+#ifndef TRIANGULATORDELAUNAY3_H
+#define TRIANGULATORDELAUNAY3_H
+
+#include "pandabase.h"
+
+/**
+ * Connects a discrete set of 3-dimensional points into tetrahedrons.
+ */
+class EXPCL_PANDA_MATHUTIL TriangulatorDelaunay3 {
+
+};
+
+#include "triangulatorDelaunay3.I"
+
+#endif // TRIANGULATORDELAUNAY3_H
