e06a025ee51ef616eb31cfb575829d8b028f80dc
Brian Lach <brianlach72@gmail.com>
2024-08-30 23:48:11 -0400

    make urchima tone mapping configurable thru prc

--- src/postprocess/toneMapping.cxx
+++ src/postprocess/toneMapping.cxx
@@ -65,7 +65,71 @@ static ConfigVariableDouble tone_mapping_urchima_contrast
 ("tone-mapping-urchima-contrast", 1.0,
  PRC_DESC("Urchima tone-mapper contrast."));
 
+static ConfigVariableDouble tone_mapping_urchima_max_display_brightness
+("tone-mapping-urchima-max-display-brightness", 1.0,
+ PRC_DESC("Urchima tone-mapper max display brightness."));
+
+static ConfigVariableDouble tone_mapping_urchima_linear_start
+("tone-mapping-urchima-linear-start", 0.22,
+ PRC_DESC("Urchima tone-mapper linear section start."));
+
+static ConfigVariableDouble tone_mapping_urchima_linear_end
+("tone-mapping-urchima-linear-end", 0.4,
+ PRC_DESC("Urchima tone-mapper linear section end."));
+
+static ConfigVariableDouble tone_mapping_urchima_black
+("tone-mapping-urchima-black", 1.33,
+ PRC_DESC("Urchima tone-mapper black."));
+
+static ConfigVariableDouble tone_mapping_urchima_pedestal
+("tone-mapping-urchima-pedestal", 0.0,
+ PRC_DESC("Urchima tone-mapper pedestal."));
+
 IMPLEMENT_CLASS(ToneMappingEffect);
+
+class ToneMappingPass : public PostProcessPass {
+public:
+  ToneMappingPass(PostProcess *pp) :
+    PostProcessPass(pp, "tone-mapping-apply") { }
+
+  virtual void update() {
+    if (tone_mapping_algorithm != TM_urchima) {
+      return;
+    }
+
+    if (_last_contrast != tone_mapping_urchima_contrast ||
+        _last_max_display_brightness != tone_mapping_urchima_max_display_brightness ||
+        _last_linear_start != tone_mapping_urchima_linear_start ||
+        _last_linear_end != tone_mapping_urchima_linear_end) {
+
+      LVecBase4 params0(tone_mapping_urchima_max_display_brightness, tone_mapping_urchima_contrast, tone_mapping_urchima_linear_start, tone_mapping_urchima_linear_end);
+      get_quad().set_shader_input("params0", params0);
+
+      _last_contrast = tone_mapping_urchima_contrast;
+      _last_max_display_brightness = tone_mapping_urchima_max_display_brightness;
+      _last_linear_start = tone_mapping_urchima_linear_start;
+      _last_linear_end = tone_mapping_urchima_linear_end;
+    }
+
+    if (_last_black != tone_mapping_urchima_black ||
+        _last_pedestal != tone_mapping_urchima_pedestal) {
+
+      LVecBase2 params1(tone_mapping_urchima_black, tone_mapping_urchima_pedestal);
+      get_quad().set_shader_input("params1", params1);
+
+      _last_black = tone_mapping_urchima_black;
+      _last_pedestal = tone_mapping_urchima_pedestal;
+    }
+  }
+
+  double _last_contrast = -1.0f;
+  double _last_max_display_brightness = -1.0f;
+  double _last_linear_start = -1.0f;
+  double _last_linear_end = -1.0f;
+
+  double _last_black = -1.0f;
+  double _last_pedestal = -1.0f;
+};
 
 /**
  *
@@ -74,7 +138,7 @@ ToneMappingEffect::
 ToneMappingEffect(PostProcess *pp) :
   PostProcessEffect(pp, "tone-mapping")
 {
-  PT(PostProcessPass) pass = new PostProcessPass(pp, "tone-mapping-apply");
+  PT(ToneMappingPass) pass = new ToneMappingPass(pp);
 
   FrameBufferProperties fbprops = pass->get_framebuffer_properties();
   fbprops.set_rgba_bits(8, 8, 8, 0); // output is LDR
@@ -104,20 +168,7 @@ ToneMappingEffect(PostProcess *pp) :
       pixel_shader));
   quad.set_shader_input("sceneTexture", pp->get_output_pipe("scene_color"));
 
-  if (tone_mapping_algorithm == TM_urchima) {
-    ///////////////////////////////////////////////////////////////
-    // Urchima properties
-    // Math: https://www.desmos.com/calculator/gslcdxvipg
-    //
-    // max display brightness, contrast, linear section start, linear section end
-    LVecBase4 params0(1.0f, tone_mapping_urchima_contrast, 0.22f, 0.4f);
-    // black, pedestal
-    LVecBase2 params1(1.33f, 0.0f);
-    ///////////////////////////////////////////////////////////////
-
-    quad.set_shader_input("params0", params0);
-    quad.set_shader_input("params1", params1);
-  }
+  pass->update();
 
   pass->add_color_output();
   pp->push_output_pipe("scene_color", pass->get_color_texture());
